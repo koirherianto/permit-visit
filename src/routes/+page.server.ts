aec30c12132beadfbfe95e3794a96f592e15ad8f
Koir herianto <koir.harianto@gmail.com>
2025-10-16 15:51:30 +0800

    bug, perbedaan datetime format

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -10,17 +10,23 @@ export const actions = {
   permitVisitSave: async ({ request, fetch }) => {
     const formData = await request.formData();
 
+    // log all
+    console.log('log semua data');
+    formData.forEach((value, key) => {
+      console.log(key, value);
+    });
+
     // 🔧 Pastikan tanggal_kunjungan aman dari format 12 jam
     const tanggalKunjungan = formData.get('tanggal_kunjungan');
 
-	if (typeof tanggalKunjungan === 'string' && tanggalKunjungan) {
-		const d = new Date(tanggalKunjungan);
-		const isoLocal = `${d.getFullYear()}-${String(d.getMonth() + 1).padStart(2, '0')}-${String(d.getDate()).padStart(2, '0')}T${String(d.getHours()).padStart(2, '0')}:${String(d.getMinutes()).padStart(2, '0')}`;
-		formData.set('tanggal_kunjungan', isoLocal);
-	}
+    if (typeof tanggalKunjungan === 'string' && tanggalKunjungan) {
+      const d = new Date(tanggalKunjungan);
+      const isoLocal = `${d.getFullYear()}-${String(d.getMonth() + 1).padStart(2, '0')}-${String(d.getDate()).padStart(2, '0')}T${String(d.getHours()).padStart(2, '0')}:${String(d.getMinutes()).padStart(2, '0')}`;
+      formData.set('tanggal_kunjungan', isoLocal);
+    }
 
     const apiUrl = PUBLIC_API_URL;
-	let laravelResponse;
+	  let laravelResponse;
 
     try {
       const response = await fetch(apiUrl + '/permit-visits', {
@@ -30,12 +36,12 @@ export const actions = {
 
 	  
       if (!response.ok) {
-		const errorText = await response.text();
-		console.error('Error:', errorText);
-		return fail(400, { message: 'Gagal kirim ke API Laravel', error: errorText });
-	  }
+        const errorText = await response.text();
+        console.error('Error:', errorText);
+        return fail(400, { message: 'Gagal kirim ke API Laravel', error: errorText });
+      }
 		
-		laravelResponse = await response.json();
+		  laravelResponse = await response.json();
     } catch (err) {
       console.error('Fetch error:', err);
       return fail(500, { message: 'Internal server error', error: err });
