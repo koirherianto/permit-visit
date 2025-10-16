318c02159df7c22f83b5129436e3850ff137d934
visarizky <visarizky@gmail.com>
2025-10-16 07:58:06 +0000

    .

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -13,14 +13,14 @@ export const actions = {
     // 🔧 Pastikan tanggal_kunjungan aman dari format 12 jam
     const tanggalKunjungan = formData.get('tanggal_kunjungan');
 
-	if (typeof tanggalKunjungan === 'string' && tanggalKunjungan) {
-		const d = new Date(tanggalKunjungan);
-		const isoLocal = `${d.getFullYear()}-${String(d.getMonth() + 1).padStart(2, '0')}-${String(d.getDate()).padStart(2, '0')}T${String(d.getHours()).padStart(2, '0')}:${String(d.getMinutes()).padStart(2, '0')}`;
-		formData.set('tanggal_kunjungan', isoLocal);
-	}
+    if (typeof tanggalKunjungan === 'string' && tanggalKunjungan) {
+      const d = new Date(tanggalKunjungan);
+      const isoLocal = `${d.getFullYear()}-${String(d.getMonth() + 1).padStart(2, '0')}-${String(d.getDate()).padStart(2, '0')}T${String(d.getHours()).padStart(2, '0')}:${String(d.getMinutes()).padStart(2, '0')}`;
+      formData.set('tanggal_kunjungan', isoLocal);
+    }
 
     const apiUrl = PUBLIC_API_URL;
-	let laravelResponse;
+    let laravelResponse;
 
     try {
       const response = await fetch(apiUrl + '/permit-visits', {
@@ -28,19 +28,19 @@ export const actions = {
         body: formData,
       });
 
-	  
+
       if (!response.ok) {
-		const errorText = await response.text();
-		console.error('Error:', errorText);
-		return fail(400, { message: 'Gagal kirim ke API Laravel', error: errorText });
-	  }
-		
-		laravelResponse = await response.json();
+        const errorText = await response.text();
+        console.error('Error:', errorText);
+        return fail(400, { message: 'Gagal kirim ke API Laravel', error: errorText });
+      }
+
+      laravelResponse = await response.json();
     } catch (err) {
       console.error('Fetch error:', err);
       return fail(500, { message: 'Internal server error', error: err });
     }
 
-	return redirect(302, '/token?token=' + laravelResponse.data.token);
+    return redirect(302, '/token?token=' + laravelResponse.data.token);
   },
 };
